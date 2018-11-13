390278a2aaff65e70cfb53bc43c79ee7d5474e2f
Samantha brake <sammy.brake@gmail.com>
2018-11-13 17:36:50 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,8 +5,12 @@ class CreateArtists < ActiveRecord::Migration
   def down
   end
 
-  def change 
+  def change
     create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
     end
-  end 
+  end
 end
